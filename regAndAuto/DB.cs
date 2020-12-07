f2bce9c096c685182e4a723c5ca80d31cd94f78a
inho-hor <olikaklacc@gmail.com>
2020-12-07 09:14:53 +0300

    добавлен класс для раоты с бд.

---
+++ regAndAuto/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regAndAuto
+{
+    class DB
+    {
+        static string connectionString = @"Data Source=LAPTOP-NLV085P5;Initial Catalog=practice;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public SqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
